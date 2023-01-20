909b4ab3f00c9853702e0a09343c980be2c0c06b
antsaare-popcore <alamaa.ants@popcore.com>
2023-01-20 14:50:40 +0100

    fixed error messages

--- Global Variables/Variables/Generic/GlobalVariableReference.cs
+++ Global Variables/Variables/Generic/GlobalVariableReference.cs
@@ -22,42 +22,48 @@ namespace AAA.GlobalVariables.Variables
             get
             {
 #if UNITY_EDITOR
-                if (Variable == null)
-                        Debug.LogError("The Variable was not set and a Constant Value will be used.");
+                if (!UseConstant && Variable == null)
+                {
+                    Debug.LogError("The Variable was not set. The Constant Value will be used instead.");
+                    UseConstant = true;
+                }
 #endif
                 return UseConstant ? ConstantValue : Variable.Value;
             }
             set
             {
 #if UNITY_EDITOR
-                    if (Variable == null)
-                        Debug.LogError("The Variable was not set.");
+                if (!UseConstant && Variable == null)
+                {
+                    Debug.LogError("The Variable was not set. The Constant Value will be used instead.");
+                    UseConstant = true;
+                }
 #endif
                 if (UseConstant)
                 {
                     var hasChanged = !ConstantValue.Equals(value);
-                    
+
                     ConstantValue = value;
-                    
-                    if(hasChanged)
+
+                    if (hasChanged)
                         OnChanged?.Invoke();
                 }
                 else
                 {
-                    //Maybe change this
+                    // Maybe change this
                     if (OnChanged != null)
                     {
                         Variable.OnChanged -= VariableOnChanged;
                         Variable.OnChanged += VariableOnChanged;
                     }
+
                     Variable.Value = value;
                 }
             }
         }
 
         void VariableOnChanged() => OnChanged?.Invoke();
-        
-        public static implicit operator TValue(GlobalVariableReference<TValue, TVariable> variable) => variable.Value;
 
+        public static implicit operator TValue(GlobalVariableReference<TValue, TVariable> variable) => variable.Value;
     }
 }
