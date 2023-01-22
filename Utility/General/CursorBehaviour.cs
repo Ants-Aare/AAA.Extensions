e665d612342cbeee86a7bfc77266799e5872421b
antsaare-popcore <alamaa.ants@popcore.com>
2023-01-22 22:57:22 +0100

    Added Cursor Behaviour

---
+++ Utility/General/CursorBehaviour.cs
@@ -0,0 +1,41 @@
+using System;
+using AAA.GlobalVariables.Variables;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace AAA.Utility.General
+{
+    public class CursorBehaviour : MonoBehaviour
+    {
+        [SerializeField] private BoolVariable variable;
+        [BoxGroup("")][SerializeField] private CursorLockMode cursorModeOnTrue;
+        [SerializeField] private bool cursorVisibleOnTrue;
+        [SerializeField] private CursorLockMode cursorModeOnFalse;
+        [SerializeField] private bool cursorVisibleOnFalse;
+
+        private void Start()
+        {
+            OnChanged();
+        }
+
+        private void OnEnable()
+        {
+            variable.OnChanged += OnChanged;
+        }
+        private void OnDisable()
+        {
+            variable.OnChanged -= OnChanged;
+        }
+
+        private void OnChanged()
+        {
+            Cursor.visible = variable
+                ? cursorVisibleOnTrue
+                : cursorVisibleOnFalse;
+            Cursor.lockState = variable
+                ? cursorModeOnTrue
+                : cursorModeOnFalse;
+        }
+    }
+}
