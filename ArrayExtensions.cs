92ced87146c09b591156e7d5bb5d5637f94fe96a
Ants-Aare <antsaare@protonmail.com>
2024-03-23 20:56:05 +0100

    array

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -41,6 +41,10 @@ namespace AAA.Extensions
 
         public static int GetIndexLooped<T>(this T[] array, int index)
         {
+            if (array == null)
+                return -1;
+            if (array.Length == 0)
+                return -1;
             return index % array.Length;
         }
 
